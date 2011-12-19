27bc85551826d0a9c9c27cbcd178cb3b2ebc436d
Brent Shepherd <thenbrent@gmail.com>
2011-12-19 15:08:07 +1000

    Better descriptions on example index.

--- index.php
+++ index.php
@@ -14,18 +14,19 @@
 <body>
 	<div class="container">
 		<h1>Thanks for Dropping by!</h1>
-		<p>This library includes three examples: <a href="example-purchase.php">Simple Purchase</a>, <a href="example-subscription.php">Simple Subscription</a> and <a href="example-subscription-checkout.php">Subscription with Separate Checkout</a>.</p>
+		<p>This library includes three examples: <a href="example-purchase.php">Simple Purchase</a>, <a href="example-subscription.php">Simple Subscription</a> and <a href="example-subscription-checkout.php">Subscription with Separate Checkout page</a>.</p>
 
 		<h3>Simple Purchase</h3>
-		<p>This example demonstrates how to use the library to accept a Digital Goods payment.</p>
+		<p>This example demonstrates how to use the library to accept a simple Digital Goods payment.</p>
 		<p><a href="example-purchase.php">Try the Simple Purchase Example &raquo;</a></p>
 
 		<h3>Simple Subscription</h3>
-		<p>Create a Digital Goods Subscription by following the this example.</p>
+		<p>Create a recurring payment by following the code for this example.</p>
 		<p><a href="example-subscription.php">Try the Simple Subscription Example &raquo;</a></p>
 
 		<h3>Subscription with Separate Checkout</h3>
-		<p>This example shows how to use a separate checkout page in the payment flow. Doing so greatly reduces the page load of your payment/sign-up page by requesting a checkout token from PayPal only after a visitor has committed to payment.</p>
+		<p>Using the library, you can also use a separate checkout page in the payment flow. Doing so greatly reduces the page load of your payment/sign-up page by requesting a checkout token from PayPal only after a visitor has committed to payment.</p>
+		<p>The code for this example demonstrates how to use a separate checkout page for a subscription sign-up.</p>
 		<p><a href="example-subscription-checkout.php">Try the Separate Checkout Example &raquo;</a></p>
 	</div>
 </body>
